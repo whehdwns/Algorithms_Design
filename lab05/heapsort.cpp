ae15da252b0c322cb6a8b368725646f3314cdc34
DJ <dongjun819@gmail.com>
2018-09-24 14:59:48 -0700

    Add files via upload

---
+++ lab05/heapsort.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <stdlib.h>
+#include <cmath>
+#include <string>
+
+using namespace std;
+//pg 154 => MAX-HEAPIFY(A,i) => MAX_HEAPIFY
+//pg 157 => BUILD-MAX-HEAP(A)=>BUILD_MAX_HEAP
+// pg 160 => HEAPSORT(A)
+
+int swap(int *x, int *y) {
+	int exchange= *x;
+	*x = *y;
+	*y = exchange;
+}
+
+int MAX_HEAPIFY(int arr[], int index, int num) {
+	//if i use <= instead of < then the number shows wrong
+	int largest = num;
+	int left = (2*num) + 1;
+	int right = (2*num) + 2;
+		if (left < index && arr[left] > arr[num]) 
+		{
+			largest = left;
+		}
+		else {
+			largest = num;
+		}
+		if (right < index && arr[right]>arr[largest]) 
+		{
+			largest = right;
+		}
+
+		if (largest != num) 
+		{
+			swap(arr[num], arr[largest]);
+			MAX_HEAPIFY(arr,index, largest);
+		}
+	}
+
+int BUILD_MAX_HEAP(int arr[], int index) {
+	//index/2-1; i>=0
+	int length = floor(int(index / 2.0));
+	for(int j=length-1; j>=0; j--)
+	{
+		MAX_HEAPIFY(arr, index, j);
+	}
+}
+	
+int HEAPSORT(int arr[], int index) {
+	BUILD_MAX_HEAP(arr, index);
+	//index-2, i>=1
+	for (int i = index-1 ; i >= 0; i--) {// if I put index-2; then the first input number sorted at the last. 
+		swap(arr[0], arr[i]);//
+		arr[index] = arr[index] - 1;
+		MAX_HEAPIFY(arr, i, 0);
+	}
+
+}
+
+void printsort(int arr[], int index) {
+	for (int i = 0; i < index; i++)
+	{
+		cout << arr[i] << ";";
+	}
+}
+
+
+int main() {
+	int index;
+	cin >> index;
+	int *arr = new int[index];
+	for (int i = 0; i < index; i++)
+		cin >> arr[i];
+	HEAPSORT(arr, index);
+	printsort(arr, index);
+	return 0;
+}
+
---
+++ lab06/Quicksort.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <stdlib.h>
+#include <cstdlib>
+//pg 171, 179 from textbook
+//pg 171 normal quicksort
+//pg 179 randomized quicksort
+using namespace std;
+
+int swap(int *x, int *y) {
+	int exchange = *x;
+	*x = *y;
+	*y = exchange;
+}
+
+int PARTITION(int arr[], int p, int r) {
+	int x = arr[r];
+	int i = p - 1;
+	for (int j = p; j <= r - 1; j++) {
+		if (arr[j] <= x) {
+			i = i + 1;
+			swap(arr[i], arr[j]);
+		}
+	}
+	swap(arr[i + 1], arr[r]);
+	return (i + 1);
+}
+
+int Randomized_Partition(int arr[], int p, int r) {
+	int i = p + rand() % (r - p + 1);
+	swap(arr[r], arr[i]);
+	return PARTITION(arr, p, r);
+}
+
+
+int Randomized_Quicksort(int arr[], int p, int r) {
+	if (p < r) {
+		int q = Randomized_Partition(arr, p, r);
+		Randomized_Quicksort(arr, p, q - 1);
+		Randomized_Quicksort(arr, q + 1, r);
+	}
+}
+
+
+void printsort(int arr[], int n) {
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << ";";
+	}
+}
+
+
+int main() {
+	int num;
+	cin >> num;
+	int *arr = new int[num];
+	for (int i = 0; i < num; i++)
+		cin >> arr[i];
+	Randomized_Quicksort(arr, 0, num - 1);
+	printsort(arr, num);
+	return 0;
+}
