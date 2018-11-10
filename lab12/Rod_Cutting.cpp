ddab6a10dad996e929a0a4a642b5690a073bd834
DJ <dongjun819@gmail.com>
2018-11-10 09:42:44 -0800

    Add files via upload

---
+++ lab12/Rod_Cutting.cpp
@@ -0,0 +1,32 @@
+//Cut_rod						pg 363
+//Memorize_cut_rod				pg 365
+//Memorize_cut_rod_aux			pg 366
+//Bottom_up_cut_rod				pg 366
+
+//Exteneded bottom up cut rod	pg 369
+//Print cut rod solution		pg 369
+
+//Used pseudocode from textbook
+//Finished 11/8/2018
+
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+
+int EXTENDED_BOTTOM_UP_CUT_ROD(int arr[], int s[], int size)
+{
+
+}
+
+
+void PRINT_CUT_ROD_SOLUTION(int arr[], int size)
+{
+
+}
+
+int main()
+{
+
+}
---
+++ lab13/DP_Matrix_Chain_Multiplication.cpp
@@ -0,0 +1,29 @@
+//matrix multiply		pg 371
+
+//Matrix_Chain - order	pg 375
+//print optimal parens	pg 377
+
+//Used pseudocode from textbook
+//Finished 11/8/2018
+
+#include <iostream>
+#include <climits>
+#include <stdlib.h>
+#include <vector>
+
+using namespace std;
+
+void PRINT_OPTIMAL_PARENS(vector<vector<int> > s, int i, int j)
+{
+
+}
+
+void MATRIX_CHAIN_ORDER(int arr[], int size)
+{
+
+}
+
+int main()
+{
+
+}
